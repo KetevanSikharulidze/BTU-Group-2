ac257d78e27afec6bb2863706e375c601952d4ba
student <student@BTU.local>
2025-03-20 16:34:36 +0400

    intial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val signInButton = findViewById<Button>(R.id.signInButton)
+
+        signInButton.setOnClickListener {
+            Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
